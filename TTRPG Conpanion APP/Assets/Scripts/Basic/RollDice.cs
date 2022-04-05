4b7af7d9126c9ed56d9365aa8b913182e3b0bbaa
O'Neil <oneil.khoshabaa@gmail.com>
2022-04-05 16:24:48 +1000

    did more shit

--- TTRPG Conpanion APP/Assets/Scripts/Basic/RollDice.cs
+++ TTRPG Conpanion APP/Assets/Scripts/Basic/RollDice.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class RollDice : MonoBehaviour
 {
     public PlayerDataSO pD;
     public int posRollAmount;
-
+    public TMP_Text resText;
 
     public void rollPosDice()
     {
@@ -16,20 +17,22 @@ public class RollDice : MonoBehaviour
         {
             int diceRis = Random.Range(1, 6);
             posRollAmount--;
-            rollResult();
+            //rollResult();
             Debug.Log(diceRis);
+            //string diceString = diceRis.ToString();
+            resText.text = diceRis.ToString();
 
         }
       
     }
 
-    void rollResult()
-    {
-        if(diceRis = 1)
-        {
+    //void rollResult()
+    //{
+    //    if(diceRis = 1)
+    //    {
 
-        }
-    }
+    //    }
+    //}
 
 
 }
