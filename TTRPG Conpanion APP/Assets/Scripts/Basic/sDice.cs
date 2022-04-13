891463d75e5c0b197202aa1e6da52cabf63e7881
O'Neil <oneil.khoshabaa@gmail.com>
2022-04-13 17:18:29 +1000

    deh

---
+++ TTRPG Conpanion APP/Assets/Scripts/Basic/sDice.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sDice : MonoBehaviour
+{
+    public GameObject Dice;
+    public GameObject diceContainer;
+    public PlayerDataSO pD;
+
+
+
+    public void spawnDice()
+    {
+        pD.numOfPosDice++;
+        Instantiate(Dice, diceContainer.transform);
+    }
+}
